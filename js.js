abb904091d0faef875efd4c15224821548422c33
TheYautja <am.96138669@gmail.com>
2024-11-11 07:57:36 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
 function mostrarola(){
     let nome = document.getElementById("nome").value;
 
-    document.getElementById("resposta").textContent = 'Olá $(nome)';
+    document.getElementById("resposta").textContent = `Olá $(nome)`;
 }
+const mostrar = document.getElementById("mostrar");
+mostrar.addEventListener("click", mostrarOla);
